798a5e2696e6fe00c833f9d63f8231817695bfb9
Dominik Jakubowski <dominikjakubowski@gmail.com>
2018-02-22 22:20:58 +0100

    Occupant edition form created.

--- src/main/java/com/homework/wspolnota/controllers/OccupantController.java
+++ src/main/java/com/homework/wspolnota/controllers/OccupantController.java
@@ -2,18 +2,26 @@ package com.homework.wspolnota.controllers;
 
 import com.homework.wspolnota.model.Flat;
 import com.homework.wspolnota.model.Occupant;
+import com.homework.wspolnota.model.Sex;
+import com.homework.wspolnota.model.repositories.FlatRepository;
 import com.homework.wspolnota.model.repositories.OccupantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class OccupantController {
 
     @Autowired
     OccupantRepository occupantRepository;
+    @Autowired
+    FlatRepository flatRepository;
 
     @GetMapping("/occupant/{id}")
     private String occupantInfo(@PathVariable("id") Long id, Model model){
@@ -28,4 +36,32 @@ public class OccupantController {
 
         return "occupant";
     }
+
+    @GetMapping("/occupant/edit/{id}")
+    private String editOccupant(@PathVariable("id") Long id, Model model){
+
+        Occupant occupant = occupantRepository.getOne(id);
+
+        model.addAttribute("occupant",occupant);
+
+        model.addAttribute("sexes", Sex.values());
+
+        List<Flat> flatList = flatRepository.findAll();
+
+        model.addAttribute("flats", flatList);
+
+
+        return "occupant_form";
+    }
+
+    @PostMapping("/occupant/edit/{id}")
+    @ResponseBody
+    private String updateOccupant(@PathVariable("id") Long id, Occupant occupant){
+
+        occupantRepository.save(occupant);
+
+
+        return "updated 2";
+    }
+
 }
